79dbcf862900a4386058c5607a01654d5a37e5af
Lightnet <darkneter@gmail.com>
2016-12-02 15:15:56 -0800

    simple cube map json file for loading into the scene.

--- src_browser/babylonjs_game.js
+++ src_browser/babylonjs_game.js
@@ -217,6 +217,7 @@ class Babylonjs_game extends Babylonjsbes6 {
 
 	createscene_assets(){
 		var self = this;
+        console.log("adding preload assets...");
 
 		this.assetsManager = new BABYLON.AssetsManager(this.scene);
 
@@ -246,8 +247,11 @@ class Babylonjs_game extends Babylonjsbes6 {
 
 		this.assetsManager.onFinish = function(tasks) {
 			console.log('assets loaded!');
-			self.setup_game();
-			self.engine.hideLoadingUI();
+			//self.setup_game();
+			//self.engine.hideLoadingUI();
+
+            self.loadmap_requestXML();
+
 		};
 		this.assetsManager.load();
 
@@ -271,6 +275,7 @@ class Babylonjs_game extends Babylonjsbes6 {
 	}
 
     loadmap_requestXML(){
+        console.log("init map json load...");
         var self = this;
         var req  = new XMLHttpRequest();
         req.open('GET', 'http://127.0.0.1/prototype.json');
@@ -298,105 +303,107 @@ class Babylonjs_game extends Babylonjsbes6 {
 		var mappdata = self.mappdata;
 		var scriptcount = self.scriptcount;
 
-			var modelfiles = [];
-			var modelcount = 0;
+		var modelfiles = [];
+		var modelcount = 0;
 
-			mappdata = JSON.parse( stringdata );
-            if(mappdata == null){
-                console.log("error!");
-                return;
-            }
-			//console.log(mappdata);
-			scriptcount = 0;
+		mappdata = JSON.parse( stringdata );
+        if(mappdata == null){
+            console.log("error!");
+            return;
+        }
+		//console.log(mappdata);
+		scriptcount = 0;
 
-			function loadmodelfiles(){
-				console.log("init models loading files");
-				if(mappdata.assets !=null){
-					console.log("Assets files: "+ mappdata.assets.length);
-					//if there no model files
-					if(mappdata.assets.length == 0){
-						loadscriptfiles();
+        loadentities();
+
+        //load 3d models and texture a
+		function loadmodelfiles(){
+			console.log("init models loading files");
+			if(mappdata.assets !=null){
+				console.log("Assets files: "+ mappdata.assets.length);
+				//if there no model files
+				if(mappdata.assets.length == 0){
+					loadscriptfiles();
+				}
+
+				for(var i = 0; i < mappdata.assets.length;i++){
+					console.log(mappdata.assets[i]);
+					if(mappdata.assets[i].type == "model"){
+						modelfiles.push(mappdata.assets[i]);
+						//modelcount += 1;
 					}
+				}
 
-					for(var i = 0; i < mappdata.assets.length;i++){
-						console.log(mappdata.assets[i]);
-						if(mappdata.assets[i].type == "model"){
-							modelfiles.push(mappdata.assets[i]);
-							//modelcount += 1;
+				//console.log("model checking...");
+				for(var mi = 0; mi < modelfiles.length;mi++){
+					//console.log(modelfiles[mi].uuid);
+					var _id = modelfiles[mi].uuid;
+					var _name = modelfiles[mi].path;
+					//console.log('//=========================');
+					//console.log(modelfiles[mi].path);
+                    /*
+					threejsapi.LoadModelFile(modelfiles[mi],(object)=>{
+						console.log('//========================================');
+						//console.log(object.name);//console.log(object.uuid);
+						console.log(object);
+						modelcount++;
+						console.log("models: "+modelcount + ":" +(modelfiles.length));
+						if(modelcount == modelfiles.length){
+							//console.log('Finish loading file models!');
+							//console.log('init scripts!');
+							loadscriptfiles();
 						}
-					}
-
-					//console.log("model checking...");
-					for(var mi = 0; mi < modelfiles.length;mi++){
-						//console.log(modelfiles[mi].uuid);
-						var _id = modelfiles[mi].uuid;
-						var _name = modelfiles[mi].path;
-						//console.log('//=========================');
-						//console.log(modelfiles[mi].path);
-                        /*
-						threejsapi.LoadModelFile(modelfiles[mi],(object)=>{
-							console.log('//========================================');
-							//console.log(object.name);//console.log(object.uuid);
-							console.log(object);
-							modelcount++;
-							console.log("models: "+modelcount + ":" +(modelfiles.length));
-							if(modelcount == modelfiles.length){
-								//console.log('Finish loading file models!');
-								//console.log('init scripts!');
-								loadscriptfiles();
-							}
-						});
-                        */
-					}
+					});
+                    */
 				}
 			}
+		}
 
-			//load scripts
-			function loadscriptfiles(){
-				if(mappdata.scripts !=null){
-					console.log("init script loading files...");
-					console.log("Scripts files: "+ mappdata.scripts.length);
-					if(mappdata.scripts.length == 0){
-						loadentities();
-					}
-					for(var i = 0; i < mappdata.scripts.length;i++){
-						//threejsapi.addScript(mappdata.scripts[i]);
-						loadScript(mappdata.scripts[i], function(){
-			    			//initialization code
-							scriptcount++;
-							//console.log("script: "+scriptcount + ":" + (mappdata.scripts.length));
-							if(scriptcount == mappdata.scripts.length){ //make sure the scripts are load else it can't used script components
-								//console.log('Finish script components!');
-								//console.log('init load entities!');
-								loadentities();
-							}
-						});
-					}
+		//load scripts
+		function loadscriptfiles(){
+			if(mappdata.scripts !=null){
+				console.log("init script loading files...");
+				console.log("Scripts files: "+ mappdata.scripts.length);
+				if(mappdata.scripts.length == 0){
+					loadentities();
+				}
+				for(var i = 0; i < mappdata.scripts.length;i++){
+					//threejsapi.addScript(mappdata.scripts[i]);
+					loadScript(mappdata.scripts[i], function(){
+		    			//initialization code
+						scriptcount++;
+						//console.log("script: "+scriptcount + ":" + (mappdata.scripts.length));
+						if(scriptcount == mappdata.scripts.length){ //make sure the scripts are load else it can't used script components
+							//console.log('Finish script components!');
+							//console.log('init load entities!');
+							loadentities();
+						}
+					});
 				}
 			}
-			//load entities
-			function loadentities(){
-				console.log('loading entities?');
-				if(mappdata.entities !=null){
-					console.log("Entities count:"+ mappdata.entities.length);
-					for(var i = 0; i < mappdata.entities.length;i++){
-						//threejsapi.parseObject(mappdata.entities[i]);
-					}
-					console.log('Finish loading!');
-
-					//self.hideloadingscreen();
-
-					//self.loadScript("/assets/test1.js", function(){
-						//initialization code
-						//console.log("test? js");
-					//});
-					//console.log(threejsapi);
+		}
+		//load entities
+		function loadentities(){
+			console.log('loading entities?');
+			if(mappdata.entities !=null){
+				console.log("Entities count:"+ mappdata.entities.length);
+				for(var i = 0; i < mappdata.entities.length;i++){
+					//threejsapi.parseObject(mappdata.entities[i]);
+                    self.parse_object(mappdata.entities[i]);
 				}
+				console.log('Finish loading!');
+
+				//self.hideloadingscreen();
+
+				//self.loadScript("/assets/test1.js", function(){
+					//initialization code
+					//console.log("test? js");
+				//});
+				//console.log(threejsapi);
 			}
-			//loadmodelfiles();
+            self.setup_game();
+		}
     }
-
-
 
 	getmesh(_name){
 		var model = null;
@@ -1369,9 +1376,7 @@ class Babylonjs_game extends Babylonjsbes6 {
 		console.log(this.engine);
         console.log(this.scene);
 		console.log(BABYLON);
-
         this.init_phsics();
-
 		this.create2DHUD();
 		//this.create2D_BattleHUD();
 		//BABYLON.DebugLayer().show();
@@ -1380,12 +1385,12 @@ class Babylonjs_game extends Babylonjsbes6 {
 		//this.createbattle_prototype();
 		this.create_input();
         this.create_gamepadinput();
-		this.create_movement();
-		this.PickObject();
-		this.simple_scene();
-        this.create_character();
+		//this.create_movement();
+		//this.PickObject();
+		//this.simple_scene();
+        //this.create_character();
 
-        this.loadmap_requestXML();
+        //this.loadmap_requestXML();
 
         //jQuery.get('http://127.0.0.1/prototype.json', function(data) {
             //console.log(data);
--- src_browser/babylonjsbes6.js
+++ src_browser/babylonjsbes6.js
@@ -168,7 +168,7 @@ class Babylonjsbes6 {
 	}
 
 	//http://doc.babylonjs.com/classes/2.4/Mesh
-    createobject(args){
+    parse_object(args){
         var _obj;
         if(args !=null){
             if(args['freecamera'] != null){
@@ -183,63 +183,70 @@ class Babylonjsbes6 {
                 _obj = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this.scene);
             }
 
-            if(args['sphere'] != null){
-                _obj = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, this.scene);
-            }
 
-            if(args['ground'] != null){
-                _obj = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, this.scene);
-            }
+			if(args['geometrytype'] !=null){
+				if(args['geometrytype'] == 'cube'){
+					_obj = this.parse_createbox(args);
+	            }
 
-            if(args['box'] != null){
-                _obj = BABYLON.Mesh.CreateBox("box", 6.0, this.scene);
-            }
+				if(args['geometrytype'] == 'sphere'){
+	                _obj = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, this.scene);
+	            }
 
-            if(args['plane'] != null){
-                _obj = BABYLON.Mesh.CreatePlane("plane", 10.0, this.scene, false, BABYLON.Mesh.DEFAULTSIDE);
-            }
+	            if(args['geometrytype'] == 'ground'){
+	                _obj = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, this.scene);
+	            }
 
-            if(args['disc'] != null){
-                _obj = BABYLON.Mesh.CreateDisc("disc", 5, 30, this.scene, false, BABYLON.Mesh.DEFAULTSIDE);
-            }
+				if(args['geometrytype'] == 'plane'){
+	                _obj = BABYLON.Mesh.CreatePlane("plane", 10.0, this.scene, false, BABYLON.Mesh.DEFAULTSIDE);
+	            }
 
-            if(args['cylinder'] != null){
-                _obj = BABYLON.Mesh.CreateCylinder("cylinder", 3, 3, 3, 6, 1, this.scene, false, BABYLON.Mesh.DEFAULTSIDE);
-            }
+	            if(args['geometrytype'] == 'disc'){
+	                _obj = BABYLON.Mesh.CreateDisc("disc", 5, 30, this.scene, false, BABYLON.Mesh.DEFAULTSIDE);
+	            }
 
-            if(args['torus'] != null){
-                _obj = BABYLON.Mesh.CreateTorus("torus", 5, 1, 10, this.scene, false, BABYLON.Mesh.DEFAULTSIDE);
-            }
+	            if(args['geometrytype'] == 'cylinder'){
+	                _obj = BABYLON.Mesh.CreateCylinder("cylinder", 3, 3, 3, 6, 1, this.scene, false, BABYLON.Mesh.DEFAULTSIDE);
+	            }
 
-            if(args['knot'] != null){
-                _obj = BABYLON.Mesh.CreateTorusKnot("knot", 2, 0.5, 128, 64, 2, 3, this.scene, false, BABYLON.Mesh.DEFAULTSIDE);
-            }
-            if(args['lines'] != null){
-                /*
-                _obj = BABYLON.Mesh.CreateLines("lines", [
-                    new BABYLON.Vector3(-10, 0, 0),
-                    new BABYLON.Vector3(10, 0, 0),
-                    new BABYLON.Vector3(0, 0, -10),
-                    new BABYLON.Vector3(0, 0, 10)
-                ], scene);
-                */
-            }
+	            if(args['geometrytype'] == 'torus'){
+	                _obj = BABYLON.Mesh.CreateTorus("torus", 5, 1, 10, this.scene, false, BABYLON.Mesh.DEFAULTSIDE);
+	            }
 
-            if(args['dashedLines'] != null){
-                //_obj = BABYLON.Mesh.CreateDashedLines("dashedLines", [v1, v2, ... vn], dashSize, gapSize, dashNb, scene);
-            }
+	            if(args['geometrytype'] == 'knot'){
+	                _obj = BABYLON.Mesh.CreateTorusKnot("knot", 2, 0.5, 128, 64, 2, 3, this.scene, false, BABYLON.Mesh.DEFAULTSIDE);
+	            }
+	            if(args['geometrytype'] == 'lines'){
+	                /*
+	                _obj = BABYLON.Mesh.CreateLines("lines", [
+	                    new BABYLON.Vector3(-10, 0, 0),
+	                    new BABYLON.Vector3(10, 0, 0),
+	                    new BABYLON.Vector3(0, 0, -10),
+	                    new BABYLON.Vector3(0, 0, 10)
+	                ], scene);
+	                */
+	            }
 
-            if(args['ribbon'] != null){
-                //_obj = BABYLON.Mesh.CreateRibbon("ribbon", [path1, path2, ..., pathn], false, false, 0, scene, false, BABYLON.Mesh.DEFAULTSIDE);
-            }
+	            if(args['dashedLines'] != null){
+	                //_obj = BABYLON.Mesh.CreateDashedLines("dashedLines", [v1, v2, ... vn], dashSize, gapSize, dashNb, scene);
+	            }
 
-            if(args['tube'] != null){
-                //_obj = BABYLON.Mesh.CreateTube("tube", [V1, V2, ..., Vn], radius, tesselation, radiusFunction, cap, scene, false, BABYLON.Mesh.DEFAULTSIDE);
-            }
+	            if(args['ribbon'] != null){
+	                //_obj = BABYLON.Mesh.CreateRibbon("ribbon", [path1, path2, ..., pathn], false, false, 0, scene, false, BABYLON.Mesh.DEFAULTSIDE);
+	            }
 
-            if(args['tiledhround'] != null){
-                //_obj = BABYLON.Mesh.CreateTiledGround("Tiled Ground", -3, -3, 3, 3, subdivisions, precision, scene, false);
-            }
+	            if(args['tube'] != null){
+	                //_obj = BABYLON.Mesh.CreateTube("tube", [V1, V2, ..., Vn], radius, tesselation, radiusFunction, cap, scene, false, BABYLON.Mesh.DEFAULTSIDE);
+	            }
+
+	            if(args['tiledhround'] != null){
+	                //_obj = BABYLON.Mesh.CreateTiledGround("Tiled Ground", -3, -3, 3, 3, subdivisions, precision, scene, false);
+	            }
+			}
+
+
+
+
 
         }else{
 
@@ -248,6 +255,40 @@ class Babylonjsbes6 {
 
         return _obj;
     }
+
+	parse_createbox(args){
+		console.log("found");
+		console.log(args);
+		var _obj = null;
+		var params = {};
+		//if(args['box'] != null){
+			//var _obj = BABYLON.MeshBuilder.CreateBox("ground", {height:1,width:20,depth:20}, this.scene);
+			//args = args['box'];
+			params.height = (typeof args['parameters']['height'] === 'number') ? args['parameters']['height'] : 1;
+			params.width = (typeof args['parameters']['width'] === 'number') ? args['parameters']['width'] : 1;
+			params.depth = (typeof args['parameters']['depth'] === 'number') ? args['parameters']['depth'] : 1;
+			var _obj = BABYLON.MeshBuilder.CreateBox("ground",params, this.scene);
+			_obj.position.x = (typeof args['position']['x'] === 'number') ? args['position']['x'] : 0;
+			_obj.position.y = (typeof args['position']['y'] === 'number') ? args['position']['y'] : 0;
+			_obj.position.z = (typeof args['position']['z'] === 'number') ? args['position']['z'] : 0;
+
+			_obj.rotation.x = (typeof args['rotation']['x'] === 'number') ? args['rotation']['x'] : 0;
+			_obj.rotation.y = (typeof args['rotation']['y'] === 'number') ? args['rotation']['y'] : 0;
+			_obj.rotation.z = (typeof args['rotation']['z'] === 'number') ? args['rotation']['z'] : 0;
+
+			_obj.scaling.x = (typeof args['scaling']['x'] === 'number') ? args['scaling']['x'] : 1;
+			_obj.scaling.y = (typeof args['scaling']['y'] === 'number') ? args['scaling']['y'] : 1;
+			_obj.scaling.z = (typeof args['scaling']['z'] === 'number') ? args['scaling']['z'] : 1;
+
+			console.log(_obj);
+
+		//}
+		return _obj;
+	}
+
+	parse_physcis(){
+
+	}
 
 	init(){
 		//console.log("init babylonjsbes6");
