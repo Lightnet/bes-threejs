866dd93f01f9394ff2b4e5fb5873a9cf7acb3ab8
Lightnet <darkneter@gmail.com>
2016-12-09 18:48:34 -0800

    working on the 2d UI test.

--- src/threejs_game/threejs_game.js
+++ src/threejs_game/threejs_game.js
@@ -205,11 +205,64 @@ export class Threejs_game extends Threejs_framework{
         this.scene.add(ui);
     }
 
+    createBaseHUD(){
+        var spriteTL, spriteTR, spriteBL, spriteBR, spriteC;
+        var self = this;
+
+
+        function updateHUDSprites () {
+            console.log(window.innerWidth);
+			var width = window.innerWidth / 2;
+			var height = window.innerHeight / 2;
+			var material = spriteTL.material;
+			var imageWidth = material.map.image.width / 2;
+			var imageHeight = material.map.image.height / 2;
+			spriteTL.position.set( - width + imageWidth,   height - imageHeight, 0 ); // top left
+			spriteTR.position.set(   width - imageWidth,   height - imageHeight, 0 ); // top right
+			spriteBL.position.set( - width + imageWidth, - height + imageHeight, 0 ); // bottom left
+			spriteBR.position.set(   width - imageWidth, - height + imageHeight, 0 ); // bottom right
+			spriteC.position.set( 0, 0, 0 ); // center
+		}
+
+
+        function createHUDSprites ( texture ) {
+			var material = new THREE.SpriteMaterial( { map: texture } );
+			var width = material.map.image.width;
+			var height = material.map.image.height;
+			spriteTL = new THREE.Sprite( material );
+			spriteTL.scale.set( width, height, 1 );
+			self.scenehud.add( spriteTL );
+
+			spriteTR = new THREE.Sprite( material );
+			spriteTR.scale.set( width, height, 1 );
+			self.scenehud.add( spriteTR );
+			spriteBL = new THREE.Sprite( material );
+			spriteBL.scale.set( width, height, 1 );
+			self.scenehud.add( spriteBL );
+			spriteBR = new THREE.Sprite( material );
+			spriteBR.scale.set( width, height, 1 );
+			self.scenehud.add( spriteBR );
+			spriteC = new THREE.Sprite( material );
+			spriteC.scale.set( width, height, 1 );
+			self.scenehud.add( spriteC );
+
+			updateHUDSprites();
+		}
+
+
+
+        var textureLoader = new THREE.TextureLoader();
+
+        var mapA = textureLoader.load( "assets/sprite0.png", createHUDSprites );
+
+    }
+
     setup(){
         this.hideloadingscreen();
         //console.log(window.width);
         //console.log(screen.width);
         console.log("setup");
+        this.createBaseHUD();
 
         //this.createbasescene();
         this.createbasescene04();
