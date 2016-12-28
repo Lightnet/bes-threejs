653c206a5fd9ea352f0f62e2f8b8df2ffbc38abe
Lightnet <darkneter@gmail.com>
2016-12-28 15:37:37 -0800

    update app. pathing?

--- js/babylonjs_requirejs_app.js
+++ js/babylonjs_requirejs_app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Configure loading modules from the lib directory,
 // except for 'app' ones, which are in a sibling
 // directory.
@@ -7,6 +5,7 @@ requirejs.config({
     baseUrl: 'lib',
     paths: {
         babylonjs_framework: '../babylonjs_framework',
+        //babylonjs_game: '../babylonjs_game',
         app: '../babylonjs_game'
     }
 });
@@ -17,22 +16,22 @@ requirejs.config({
 // your application logic in there.
 //requirejs(['./app/babylonjs_game_boot']);
 var BABYLONJSAPI;
-function Call() {
+function CallAPI(){
     console.log(BABYLONJSAPI);
 }
 
-requirejs(['./app/babylonjs_game'], (function (app) {
-    console.log(app);
+requirejs(['./app/Babylonjs_game'],(app)=>{
+    //console.log(app);
     var BGame = new app.Babylonjs_game();
-    console.log(BGame);
+    //console.log(BGame);
     BGame.init();
     BABYLONJSAPI = BGame;
-    Call();
-}));
+    //CallAPI();
+});
 
-requirejs(['./app/gmodule'], (function (app) {
+//requirejs(['./app/system/gmodule'],(app)=>{
     //console.log(app.default['value']);
     //console.log(app.default);
-}));
+//});
 
-console.log(BABYLONJSAPI);
+//console.log(BABYLONJSAPI);
